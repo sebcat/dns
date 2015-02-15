0bf16f6f50b30668728b29c4821612468fee7104
Sebastian Cato <sc@outpost24.com>
2015-02-15 15:52:31 +0100

    removed blank lines for go fmt

--- dnsscanner/protocol.go
+++ dnsscanner/protocol.go
@@ -9,60 +9,55 @@ import (
 
 // Types
 const (
-	A     uint16 = 1
-	NS           = 2
-	MD           = 3
-	MF           = 4
-	CNAME        = 5
-	SOA          = 6
-	MB           = 7
-	MG           = 8
-	MR           = 9
-	NULL         = 10
-	WKS          = 11
-	PTR          = 12
-	HINFO        = 13
-	MINFO        = 14
-	MX           = 15
-	TXT          = 16
-
-	RP    = 17
-	AFSDB = 18
-	SIG   = 24
-	KEY   = 25
-
-	AAAA = 28
-
-	LOC        = 29
-	SRV        = 33
-	NAPTR      = 35
-	KX         = 36
-	CERT       = 37
-	DNAME      = 39
-	APL        = 42
-	DS         = 43
-	SSHFP      = 44
-	IPSECKEY   = 45
-	RRSIG      = 46
-	NSEC       = 47
-	DNSKEY     = 48
-	DHCID      = 49
-	NSEC3      = 50
-	NSEC3PARAM = 51
-	TLSA       = 52
-	HIP        = 55
-	CDS        = 59
-	CDNSKEY    = 60
-	TKEY       = 249
-	TSIG       = 250
-
-	AXFR  = 252
-	MAILB = 253
-	MAILA = 254
-
-	CAA = 257
-	TA  = 32768
-	DLV = 32769
+	A          uint16 = 1
+	NS                = 2
+	MD                = 3
+	MF                = 4
+	CNAME             = 5
+	SOA               = 6
+	MB                = 7
+	MG                = 8
+	MR                = 9
+	NULL              = 10
+	WKS               = 11
+	PTR               = 12
+	HINFO             = 13
+	MINFO             = 14
+	MX                = 15
+	TXT               = 16
+	RP                = 17
+	AFSDB             = 18
+	SIG               = 24
+	KEY               = 25
+	AAAA              = 28
+	LOC               = 29
+	SRV               = 33
+	NAPTR             = 35
+	KX                = 36
+	CERT              = 37
+	DNAME             = 39
+	APL               = 42
+	DS                = 43
+	SSHFP             = 44
+	IPSECKEY          = 45
+	RRSIG             = 46
+	NSEC              = 47
+	DNSKEY            = 48
+	DHCID             = 49
+	NSEC3             = 50
+	NSEC3PARAM        = 51
+	TLSA              = 52
+	HIP               = 55
+	CDS               = 59
+	CDNSKEY           = 60
+	TKEY              = 249
+	TSIG              = 250
+	AXFR              = 252
+	MAILB             = 253
+	MAILA             = 254
+	CAA               = 257
+	TA                = 32768
+	DLV               = 32769
 )
 
 //Classes
