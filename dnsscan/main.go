2f702222694a9120d836aff0b9616d5b1e61ee18
Sebastian Cato <sc@outpost24.com>
2015-02-15 11:42:55 +0100

    Added basic, unbounded concurrency to dnsscan

--- dnsscan/main.go
+++ dnsscan/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"flag"
 	"github.com/sebcat/dns/dnsscanner"
 	"log"
 	"net"
+	"sync"
+	"time"
 )
 
-func main() {
-	conn, err := net.Dial("udp", "8.8.8.8:53")
+var (
+	nsaddr  = flag.String("ns", "8.8.8.8:53", "DNS server")
+	nsproto = flag.String("proto", "udp", "transport layer protocol")
+	ttl     = flag.Duration("timeout", 10*time.Second, "dial and connection timeout")
+)
+
+func lookup(wg *sync.WaitGroup, proto, addr, name string, timeout time.Duration) {
+	defer wg.Done()
+	conn, err := net.DialTimeout(proto, addr, timeout)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	defer conn.Close()
 
-	m := dnsscanner.NewQuery("www.detectify.com", dnsscanner.ANY, dnsscanner.IN)
-
+	m := dnsscanner.NewQuery(name, dnsscanner.ANY, dnsscanner.IN)
 	if err := m.Send(conn); err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("no names given")
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(args))
+	for _, name := range flag.Args() {
+		go lookup(&wg, *nsproto, *nsaddr, name, *ttl)
+	}
+
+	wg.Wait()
 }
